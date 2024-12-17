eba02fee00f1217012b84188b68df6493829720c
Esther <estherfinsen@gmail.com>
2024-12-17 11:36:30 +0100

    gs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,9 +27,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="./favicon.ico" sizes="any" />
-      </head>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+
       <body className={`${work_header.className} bg-blue m-0`}>
         <header className="w-full">
           <Navbar />
