a6e1fa30ebf504b6ee16cf2fb1c7ce84dfc18b53
Esther <estherfinsen@gmail.com>
2024-12-17 11:10:48 +0100

    gs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
       </head>
       <body className={`${work_header.className} bg-blue m-0`}>
         <header className="w-full">
