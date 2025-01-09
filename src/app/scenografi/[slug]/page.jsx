1a3a8436d0604d8472bbecd2f5b2905646455cd7
Esther <estherfinsen@gmail.com>
2025-01-09 13:51:04 +0100

    gg

--- src/app/scenografi/[slug]/page.jsx
+++ src/app/scenografi/[slug]/page.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import { Work_Sans } from "next/font/google";
-import dataGrafik from "@/app/dataGrafik";
+import dataScenografi from "@/app/dataScenografi";
 
-const work_bread = Work_Sans({
-  weight: ["500"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-});
 const work_header = Work_Sans({
   weight: ["900"],
   style: ["normal", "italic"],
@@ -16,9 +10,9 @@ const work_header = Work_Sans({
   display: "swap",
 });
 
-export default async function singleGrafik({ params }) {
+export default async function singleScenografi({ params }) {
   const { slug } = await params;
-  const response = await fetch(`https://jhgcdsutaztszxtyehtm.supabase.co/rest/v1/grafik?slug=eq.${slug}`, {
+  const response = await fetch(`https://jhgcdsutaztszxtyehtm.supabase.co/rest/v1/scenografi?slug=eq.${slug}`, {
     method: "GET",
     headers: {
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoZ2Nkc3V0YXp0c3p4dHllaHRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMTg0NTgsImV4cCI6MjA0NzU5NDQ1OH0.VVF1QVqChKO2sUcYjYOuaUFdGCe68Sj3B2ZxG05mQiI",
@@ -27,21 +21,21 @@ export default async function singleGrafik({ params }) {
 
   const res = await response.json();
   const data = res[0];
-  const grafik = dataGrafik.filter((item) => item.id === data.id);
+  const scenografi = dataScenografi.filter((item) => item.id === data.id);
 
   return (
-    <section className={`${work_header.className} text-red uppercase  text-headers relative`}>
+    <section className={`${work_header.className} text-red uppercase text-headers relative`}>
       <div className="relative overflow-hidden w-screen">
         <div className="absolute top-0 left-0 w-full h-[2px] bg-red"></div>
         <div className="relative flex animate-program-2 w-screen">
-          <p className="text-running flex-shrink-0 tracking-widest word-spacing-custom py-3">grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik grafik</p>
+          <p className="text-running flex-shrink-0 tracking-widest word-spacing-custom py-3">scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi scenografi</p>
         </div>
         <div className="absolute bottom-0 left-0 w-full h-[2px] bg-red"></div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-screen px-0 pt-20 work_bread">
-        <div className="md:col-start-1 md:row-start-1 relative w-full pb-[100%] overflow-hidden mt-3">
-          <Image src={grafik[0].images[0].src} alt={grafik[0].images[0].alt || "Default alt text"} layout="fill" objectFit="cover" />
+        <div className="md:col-start-1 md:row-start-1 relative w-full pb-[100%]  overflow-hidden mt-3">
+          <Image src={scenografi[0].images[0].src} alt={scenografi[0].images[0].alt || "Default alt text"} layout="fill" objectFit="cover" />
         </div>
 
         <div className="md:col-start-2 md:row-start-1 px-4 md:px-8 flex flex-col">
@@ -49,8 +43,8 @@ export default async function singleGrafik({ params }) {
           <p className="font-medium mt-4 text-running md:text-running lg:text-breads">{data.description}</p>
         </div>
 
-        {grafik[0].images.slice(1).map((image, i) => (
-          <div key={i} className="relative w-full pb-[100%] overflow-hidden">
+        {scenografi[0].images.slice(1).map((image, i) => (
+          <div key={i} className="relative w-full pb-[100%]  overflow-hidden">
             <Image src={image.src} alt={image.alt || `Image ${i + 1}`} layout="fill" objectFit="cover" />
           </div>
         ))}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,30 +29,34 @@ const Navbar = () => {
   return (
     <nav className="flex items-center gap-8 md:gap-24 lg:gap-32">
       <Link href="/" className="flex items-center">
-        <p className={`${work_header.className} uppercase text-nav_mobile md:text-nav_ipad lg:text-nav text-red`}>E M D F </p>
+        <p className={`${work_header.className} uppercase text-nav_mobile md:text-nav_ipad lg:text-nav text-red`}>Finsens</p>
       </Link>
 
       <div className="flex items-end z-30 scale-x-[1.3] scale-y-[1.3] md:scale-x-[2.9] md:scale-y-[2.9] pt-0.5 lg:scale-x-[4.6] lg:scale-y-[4.6]">
-        <Hamburger toggled={isOpen} toggle={setOpen} color={isOpen ? "#E3FFD9" : "#FF2626"} size={48} />
+        <Hamburger toggled={isOpen} toggle={setOpen} color={isOpen ? "#E1F7FF" : "#FF4125"} size={48} />
       </div>
 
       {isOpen && (
         <div className="w-full h-full bg-red z-20 absolute flex inset-0 justify-center items-center">
           <ul className="flex flex-col justify-center items-center self-center text-blue uppercase gap-6">
             <li onClick={() => setOpen(false)}>
-              <Link href="/cases">
-                <p className="hover:underline text-2xl">Cases</p>
+              <Link href="/grafik">
+                <p className="hover:underline text-2xl">Grafik</p>
               </Link>
             </li>
-
+            <li onClick={() => setOpen(false)}>
+              <Link href="/scenografi">
+                <p className="hover:underline text-2xl">Scenografi</p>
+              </Link>
+            </li>
             <li onClick={() => setOpen(false)}>
               <Link href="/speak">
-                <p className="hover:underline text-2xl">about</p>
+                <p className="hover:underline text-2xl">Speak</p>
               </Link>
             </li>
             <li onClick={() => setOpen(false)}>
               <Link href="/kontakt">
-                <p className="hover:underline text-2xl">contact</p>
+                <p className="hover:underline text-2xl">Kontakt</p>
               </Link>
             </li>
           </ul>
